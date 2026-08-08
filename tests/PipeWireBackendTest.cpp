6fe8f3c03ec22cce61824683a460df66dda5a97c
Shovy <1826784860@qq.com>
2026-08-08 15:51:19 +0800

    tests(pipewire): add PipeWireBackendTest

--- tests/PipeWireBackendTest.cpp
+++ tests/PipeWireBackendTest.cpp
@@ -1,1 +1,50 @@
-#include <AudioConducker/pipewwire/PipeWireBackend>
+#include <gtest/gtest.h>
+#include <thread>
+#include <chrono>
+
+#include "AudioConducker/pipewire/PipeWireContext.h"
+#include "AudioConducker/pipewire/PipeWireBackend.h"
+
+using namespace AudioConducker;
+
+TEST(PipeWireBackendTest, DiscoverStreams){
+    PipeWireContext context;
+    PipeWireBackend backend(context);
+
+    std::thread loop([&](){
+        pw_main_loop_run(context.getMainLoop());
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    auto streams = backend.getStreams();
+
+    EXPECT_GT(streams.size(), 0);
+
+    pw_main_loop_quit(context.getMainLoop());
+
+    loop.join();
+}
+
+TEST(PipeWireBackendTest, SetVolume){
+    PipeWireContext context;
+    PipeWireBackend backend(context);
+
+    std::thread loop([&]() {
+            pw_main_loop_run(context.getMainLoop());
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    auto streams = backend.getStreams();
+
+    ASSERT_FALSE(streams.empty());
+
+    auto id = streams[0].id;
+
+    backend.setVolume(id, 0.2f);
+
+    pw_main_loop_quit(context.getMainLoop());
+
+    loop.join();
+}
